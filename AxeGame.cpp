4a9aa3b06806bba00464d8099d3285b89542457c
IneshAg <ineshag123@gmail.com>
2025-10-01 15:51:31 +0530

    .

--- AxeGame.cpp
+++ AxeGame.cpp
@@ -1,27 +1,38 @@
 #include "raylib.h"
 int main()
 {
-    int width{350};
-    int height{200};
+    int width{800};
+    int height{400};
     InitWindow(width, height, "Steve");
-    int circle_x{175};
-    int circle_y{100};
+    int circle_x{200};
+    int circle_y{200};
     int radius{30};
+
+    int axe_x{400};
+    int axe_y{0};
+
+    int direction{10};
     SetTargetFPS(60);
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(BLUE);
         DrawCircle(circle_x, circle_y, radius, RED);
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+        axe_y += direction;
+        if (axe_y > height || axe_y < 0)
 
+        {
+            direction = -direction;
+        }
         if (IsKeyDown(KEY_D) && circle_x < width)
         {
             circle_x += 10;
         }
-        if (IsKeyDown(KEY_A) &&circle_x > 0)
-            {
-                circle_x -= 10;
-            }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x -= 10;
+        }
 
         EndDrawing();
     }
